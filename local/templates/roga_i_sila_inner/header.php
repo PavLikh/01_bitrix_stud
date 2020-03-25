32b4bac205593f2f7748ecb340ed8dbc5df3b24c
likhopoy.p <student3@qsoft.ru>
2020-03-25 10:02:44 +0000

    131213 / Интеграция верстки левого меню - добавил шаблон меню menu_left - подключил шаблон на странице

--- local/templates/roga_i_sila_inner/header.php
+++ local/templates/roga_i_sila_inner/header.php
@@ -11,14 +11,31 @@
 		<nav>
 		<ul class="left_menu">
 				<li>
-					<span>Информация</span>
-					<ul>
+					<span><?=\Bitrix\Main\Localization\Loc::getMessage('INFORMATION')?></span>
+<!-- 					<ul>
 						<li><a href="#">О компании</a></li>
 						<li><a href="#" class="selected">Контактная информация</a></li>
 						<li><a href="#">Условия продаж</a></li>
 						<li><a href="#">Финансовый отдел</a></li>
 						<li><a href="#">Для клиентов</a></li>
-					</ul>
+					</ul> -->
+					<?$APPLICATION->IncludeComponent(
+						"bitrix:menu",
+						"menu_left",
+						Array(
+							"ALLOW_MULTI_SELECT" => "N",
+							"CHILD_MENU_TYPE" => "left",
+							"COMPONENT_TEMPLATE" => "menu_left",
+							"DELAY" => "N",
+							"MAX_LEVEL" => "1",
+							"MENU_CACHE_GET_VARS" => array(),
+							"MENU_CACHE_TIME" => "3600",
+							"MENU_CACHE_TYPE" => "A",
+							"MENU_CACHE_USE_GROUPS" => "Y",
+							"ROOT_MENU_TYPE" => "bottom",
+							"USE_EXT" => "N"
+						)
+					);?>
 				</li>
 			</ul> 
 		</nav>
