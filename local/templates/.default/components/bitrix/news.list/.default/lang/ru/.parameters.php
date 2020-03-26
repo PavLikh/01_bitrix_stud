eed85c4d4c6e24b94e1e0f4107a24bf15501c340
likhopoy.p <student3@qsoft.ru>
2020-03-26 08:37:09 +0000

    131214 / Разработка блока "Наши малоны" в футере - Промежуточный коммит с тестовыми наработками

---
+++ local/templates/.default/components/bitrix/news.list/.default/lang/ru/.parameters.php
@@ -0,0 +1,8 @@
+<?
+$MESS ['T_IBLOCK_DESC_NEWS_DATE'] = "Выводить дату элемента";
+$MESS ['T_IBLOCK_DESC_NEWS_NAME'] = "Выводить название элемента";
+$MESS ['T_IBLOCK_DESC_NEWS_PICTURE'] = "Выводить изображение для анонса";
+$MESS ['T_IBLOCK_DESC_NEWS_TEXT'] = "Выводить текст анонса";
+$MESS ['LIST_PREV_PICT_H'] = "Высота уменьшенного изображения";
+$MESS ['LIST_PREV_PICT_W'] = "Ширина уменьшенного изображения";
+?>
