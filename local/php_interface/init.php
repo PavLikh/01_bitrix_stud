8c160b4fded1290a4adb6d3bec8ed2fb5fe0d88c
likhopoy.p <student3@qsoft.ru>
2020-04-01 18:34:15 +0000

    131217 / Контрольная задача - Исправил типовые ошибки

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -8,4 +8,31 @@ if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/event_han
 if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/test_handler.php"))
 	require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/test_handler.php");
 
+
+AddEventHandler("main", "OnAfterUserAuthorize", Array("MyClass", "OnAfterUserAuthorizeHandler"));
+
+class MyClass
+{
+    // создаем обработчик события "OnAfterUserAuthorize"
+    public static function OnAfterUserAuthorizeHandler($arUser)
+    {
+
+
+    	CEvent::Send(
+			 'AUTHORIZATION',
+			 SITE_ID,
+			 array(
+				'USER_LOGIN' => $arUser['user_fields']['LOGIN'],
+	    		//'SITE_NAME' => $arUser['user_fields']['LID'],
+	    		'USER_MAIL' => $arUser['user_fields']['EMAIL'],
+	    		'LAST_AUTH_TIME' => date('Y.d.m H:i:s'),
+			 )
+
+		);
+    }
+}
+
+
+
+
 ?>
